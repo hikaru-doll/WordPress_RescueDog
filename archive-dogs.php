e33a97bf08dbdfcd1743f420c04986c2bcd43c8d
hikaru <makinaminami@gmail.com>
2026-07-27 12:35:53 +0900

    サブクエリのループにする

--- archive-dogs.php
+++ archive-dogs.php
@@ -1,257 +1,58 @@
-<!doctype html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Rescue Dog</title>
-    <link rel="stylesheet" href="assets/css/variable.css" />
-    <link rel="stylesheet" href="assets/css/reset.css" />
-    <link rel="stylesheet" href="style.css" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-    <link rel="stylesheet" href="responsive.css" />
-  </head>
-  <body>
-    <!-- header ----------------------->
-    <header class="header flex">
-      <!-- <div class="flex"> -->
-      <div class="flex">
-        <h1 class="logo">
-          <a href="#">
-            <img src="assets/img/RescueDog.png" alt="レスキュードッグのロゴ" />
-          </a>
-        </h1>
-        <ul class="reception">
-          <li>電話番号:<a href="tel:0000-00-0000">0000-00-0000</a></li>
-          <li>受付時間:午前9時～午後6時</li>
-        </ul>
-      </div>
-      <nav id="header-nav" class="header-nav">
-        <ul class="nav-menu flex">
-          <li><a href="#">トップ</a></li>
-          <li><a href="#"> ワンちゃん紹介 </a></li>
-          <li>
-            <a href="">譲渡について</a>
-            <ul class="sub-menu">
-              <li><a href="#b">譲渡の流れ</a></li>
-              <li><a href="#c   ">譲渡の条件</a></li>
-            </ul>
-          </li>
-          <li><a href="#">ご支援について</a></li>
-          <li><a href="#">私たちについて</a></li>
-          <li><a href="#">お問い合わせ</a></li>
-        </ul>
-      </nav>
-      <nav id="open-nav" class="open-nav">
-        <ul class="nav-menu flex">
-          <li><a href="#">トップぺージ</a></li>
-          <li><a href="#">ワンちゃん紹介</a></li>
-          <li>
-            譲渡について
-            <ul class="sub-menu">
-              <li><a href="#">譲渡の流れ</a></li>
-              <li><a href="#">譲渡の条件</a></li>
-            </ul>
-          </li>
-          <li><a href="#">ご支援について</a></li>
-          <li><a href="#">私たちについて</a></li>
-          <li><a href="#">お問い合わせ</a></li>
-        </ul>
-      </nav>
-      <button
-        class="ham"
-        aria-controls="open-nav"
-        aria-expanded="false"
-        aria-label="メニューを開く"
-      >
-        <span class="ham1"></span>
-        <span class="ham2"></span>
-        <span class="ham3"></span>
-      </button>
-      <!-- </div> -->
-    </header>
-    <!-- main ------------------------->
-    <main class="archive-dogs main">
-      <div class="wrapper">
-        <h1 class="title">ワンちゃん紹介</h1>
-        <ul class="grid lists">
+<?php
+defined('ABSPATH') || exit;
+?>
+<?php get_header(); ?>
+<!-- main ------------------------->
+<main class="archive-dogs main">
+  <div class="wrapper">
+    <h1 class="title">ワンちゃん紹介</h1>
+    <ul class="grid lists">
+
+      <?php
+      $dogs_posts = new WP_Query(array(
+        'post_type'      => 'dogs', // ★デフォルト投稿
+        'posts_per_page' => -1,
+        'post_status'    => 'publish',
+        'orderby'        => 'date',
+        'order'          => 'DESC',
+      ));
+
+      if ($dogs_posts->have_posts()) :
+        while ($dogs_posts->have_posts()) : $dogs_posts->the_post();
+      ?>
+
+
+
           <li class="item">
-            <a href="#">
+            <a href="<?php the_permalink(); ?>">
               <div class="image">
-                <img src="assets/img/volunteer.webp" alt="ワンコ" />
+                <?php the_post_thumbnail(); ?>
               </div>
               <div class="text">
-                <h2 class="name">リリー</h2>
+                <h2 class="name"><?php the_title(); ?></h2>
                 <ul class="attribute flex">
-                  <li>年齢：推定5歳</li>
-                  <li>性別：女の子</li>
+                  <li>年齢：<?php the_field("age"); ?></li>
+                  <li>性別：<?php the_field("sex"); ?></li>
                 </ul>
                 <p class="content">
-                  ワンちゃん用のおやつよりもリンゴが大好きな女の子。とてもやさしくて、新参者の面倒…
+                  <?php echo esc_html(wp_trim_words(get_field('profile'), 35, '…')); ?>
                 </p>
-                <p class="link">リリーをもっと知る</p>
+                <p class="link"><?php the_title(); ?>をもっと知る</p>
               </div>
             </a>
           </li>
-          <!-- 後で消すこっから************************************************************************** -->
-          <li class="item">
-            <a href="#">
-              <div class="image">
-                <img src="assets/img/volunteer.webp" alt="ワンコ" />
-              </div>
-              <div class="text">
-                <h2 class="name">リリー</h2>
-                <dl class="attribute flex">
-                  <dt>年齢：</dt>
-                  <dd>推定5歳</dd>
-                  <dt>性別：</dt>
-                  <dd>女の子</dd>
-                </dl>
-                <p class="content">
-                  ワンちゃん用のおやつよりもリンゴが大好きな女の子。とてもやさしくて、新参者の面倒…
-                </p>
-                <p class="link">リリーをもっと知る</p>
-              </div>
-            </a>
-          </li>
-          <li class="item">
-            <a href="#">
-              <div class="image">
-                <img src="assets/img/volunteer.webp" alt="ワンコ" />
-              </div>
-              <div class="text">
-                <h2 class="name">リリー</h2>
-                <dl class="attribute flex">
-                  <dt>年齢：</dt>
-                  <dd>推定5歳</dd>
-                  <dt>性別：</dt>
-                  <dd>女の子</dd>
-                </dl>
-                <p class="content">
-                  ワンちゃん用のおやつよりもリンゴが大好きな女の子。とてもやさしくて、新参者の面倒…
-                </p>
-                <p class="link">リリーをもっと知る</p>
-              </div>
-            </a>
-          </li>
-          <li class="item">
-            <a href="#">
-              <div class="image">
-                <img src="assets/img/volunteer.webp" alt="ワンコ" />
-              </div>
-              <div class="text">
-                <h2 class="name">リリー</h2>
-                <dl class="attribute flex">
-                  <dt>年齢：</dt>
-                  <dd>推定5歳</dd>
-                  <dt>性別：</dt>
-                  <dd>女の子</dd>
-                </dl>
-                <p class="content">
-                  ワンちゃん用のおやつよりもリンゴが大好きな女の子。とてもやさしくて、新参者の面倒…
-                </p>
-                <p class="link">リリーをもっと知る</p>
-              </div>
-            </a>
-          </li>
-          <li class="item">
-            <a href="#">
-              <div class="image">
-                <img src="assets/img/volunteer.webp" alt="ワンコ" />
-              </div>
-              <div class="text">
-                <h2 class="name">リリー</h2>
-                <dl class="attribute flex">
-                  <dt>年齢：</dt>
-                  <dd>推定5歳</dd>
-                  <dt>性別：</dt>
-                  <dd>女の子</dd>
-                </dl>
-                <p class="content">
-                  ワンちゃん用のおやつよりもリンゴが大好きな女の子。とてもやさしくて、新参者の面倒…
-                </p>
-                <p class="link">リリーをもっと知る</p>
-              </div>
-            </a>
-          </li>
-          <li class="item">
-            <a href="#">
-              <div class="image">
-                <img src="assets/img/volunteer.webp" alt="ワンコ" />
-              </div>
-              <div class="text">
-                <h2 class="name">リリー</h2>
-                <dl class="attribute flex">
-                  <dt>年齢：</dt>
-                  <dd>推定5歳</dd>
-                  <dt>性別：</dt>
-                  <dd>女の子</dd>
-                </dl>
-                <p class="content">
-                  ワンちゃん用のおやつよりもリンゴが大好きな女の子。とてもやさしくて、新参者の面倒…
-                </p>
-                <p class="link">リリーをもっと知る</p>
-              </div>
-            </a>
-          </li>
-          <li class="item">
-            <a href="#">
-              <div class="image">
-                <img src="assets/img/volunteer.webp" alt="ワンコ" />
-              </div>
-              <div class="text">
-                <h2 class="name">リリー</h2>
-                <ul class="attribute flex">
-                  <li>年齢：推定5歳</li>
-                  <li>性別：女の子</li>
-                </ul>
-                <p class="content">
-                  ワンちゃん用のおやつよりもリンゴが大好きな女の子。とてもやさしくて、新参者の面倒…
-                </p>
-                <p class="link">リリーをもっと知る</p>
-              </div>
-            </a>
-          </li>
-          <!-- 後で消す ここまで************************************************************************* -->
-        </ul>
-      </div>
-      <div>
-        <p class="pagenation">pagenation</p>
-      </div>
-    </main>
-    <!-- footer ----------------------->
-    <footer class="footer">
-      <div class="flex wrapper">
-        <div class="item">
-          <img
-            class="logo"
-            src="assets/img/RescueDog.png"
-            alt="レスキュードッグのロゴ"
-          />
-          <dl class="reception grid">
-            <dt>電話番号：</dt>
-            <dd><a href="tel:09000000000">090-0000-0000</a></dd>
-            <dt>受付時間：</dt>
-            <dd>午前9時～午後6時</dd>
-          </dl>
-        </div>
-        <nav id="footer-nav" class="footer-nav">
-          <ul>
-            <li><a href="#">トップページ</a></li>
-            <li><a href="#">ワンちゃん紹介</a></li>
-            <li><a href="#">譲渡の条件</a></li>
-            <li><a href="#">譲渡の流れ</a></li>
-            <li><a href="#">お知らせ一覧ページ</a></li>
-            <li><a href="#">私たちについて</a></li>
-            <li><a href="#">ご支援について</a></li>
-            <li><a href="#">ボランティアさん募集</a></li>
-            <li><a href="#">お問い合わせ</a></li>
-            <li><a href="#">収支報告一覧ページ</a></li>
-            <li><a href="#">プライバシーポリシー</a></li>
-          </ul>
-        </nav>
-      </div>
-      <p class="copyright">&copy;<span id="year"></span> RESCUE DOG</p>
-    </footer>
-    <script src="main.js"></script>
-  </body>
-</html>
+
+        <?php
+        endwhile;
+        wp_reset_postdata(); // ← 必須！
+      else :
+        ?>
+        <p>投稿がありません。</p>
+      <?php endif; ?>
+    </ul>
+  </div>
+  <div>
+    <p class="pagenation">pagenation</p>
+  </div>
+</main>
+<?php get_footer(); ?>
