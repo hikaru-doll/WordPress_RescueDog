0d822ecbd379c9c8788b5ec3f455c2d68382d6a2
hikaru <makinaminami@gmail.com>
2026-07-27 13:49:54 +0900

    サブクエリでループする

--- home.php
+++ home.php
@@ -1,221 +1,138 @@
-<!doctype html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Rescue Dog</title>
-    <link rel="stylesheet" href="assets/css/variable.css" />
-    <link rel="stylesheet" href="assets/css/reset.css" />
-    <link rel="stylesheet" href="style.css" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-    <link rel="stylesheet" href="responsive.css" />
-  </head>
-  <body>
-    <!-- header ----------------------->
-    <header class="header flex">
-      <!-- <div class="flex"> -->
-      <div class="flex">
-        <h1 class="logo">
-          <a href="#">
-            <img src="assets/img/RescueDog.png" alt="レスキュードッグのロゴ" />
-          </a>
-        </h1>
-        <ul class="reception">
-          <li>電話番号:<a href="tel:0000-00-0000">0000-00-0000</a></li>
-          <li>受付時間:午前9時～午後6時</li>
-        </ul>
-      </div>
-      <nav id="header-nav" class="header-nav">
-        <ul class="nav-menu flex">
-          <li><a href="#">トップ</a></li>
-          <li><a href="#"> ワンちゃん紹介 </a></li>
-          <li>
-            <a href="">譲渡について</a>
-            <ul class="sub-menu">
-              <li><a href="#b">譲渡の流れ</a></li>
-              <li><a href="#c   ">譲渡の条件</a></li>
+<?php
+defined('ABSPATH') || exit;
+?>
+<?php get_header(); ?>
+<!-- main ------------------------->
+<main class="home-php main">
+  <div class="bg">
+    <h1 class="title">お知らせ一覧</h1>
+    <ul class="lists">
+      <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+          <li class="content">
+            <div class="image">
+              <a href="<?php the_permalink(); ?>">
+                <?php if (has_post_thumbnail()): ?>
+                  <?php the_post_thumbnail(); ?>
+                <?php endif; ?>
+            </div>
+            <h2 class="dog-name"> <?php the_title(); ?></h2>
+            <ul class="attribute flex">
+              <li>年齢：<?php the_field('age'); ?></li>
+              <li>性別：<?php the_field('sex'); ?></li>
             </ul>
-          </li>
-          <li><a href="#">ご支援について</a></li>
-          <li><a href="#">私たちについて</a></li>
-          <li><a href="#">お問い合わせ</a></li>
-        </ul>
-      </nav>
-      <nav id="open-nav" class="open-nav">
-        <ul class="nav-menu flex">
-          <li><a href="#">トップぺージ</a></li>
-          <li><a href="#">ワンちゃん紹介</a></li>
-          <li>
-            譲渡について
-            <ul class="sub-menu">
-              <li><a href="#">譲渡の流れ</a></li>
-              <li><a href="#">譲渡の条件</a></li>
-            </ul>
-          </li>
-          <li><a href="#">ご支援について</a></li>
-          <li><a href="#">私たちについて</a></li>
-          <li><a href="#">お問い合わせ</a></li>
-        </ul>
-      </nav>
-      <button
-        class="ham"
-        aria-controls="open-nav"
-        aria-expanded="false"
-        aria-label="メニューを開く"
-      >
-        <span class="ham1"></span>
-        <span class="ham2"></span>
-        <span class="ham3"></span>
-      </button>
-      <!-- </div> -->
-    </header>
-    <!-- main ------------------------->
-    <main class="home-php main">
-      <div class="bg">
-        <h1 class="title">お知らせ一覧</h1>
-        <ul class="lists">
-          <li class="item">
-            <a href="#">
-              <div class="grid inner">
-                <div class="image">
-                  <img src="assets/img/volunteer.webp" alt="" />
-                </div>
-                <div class="text">
-                  <h2 class="item-title">ボランティアさんと散歩へ</h2>
-                  <p class="date">2026.05.24</p>
-                  <p class="content">
-                    今日は大学生のボランティアさんがたくさん遊んでくれました！
-                  </p>
-                  <p class="link">続きを読む</p>
-                </div>
-              </div>
+            <p class="profile"> <?php echo esc_html(wp_trim_words(get_field('profile'), 40, '…')); ?></p>
+            <p class="read-more"> <?php the_title(); ?>をもっと知る</p>
             </a>
           </li>
-          <!-- delete later from here*************************************************-->
-          <li class="item">
-            <a href="#">
-              <div class="grid inner">
-                <div class="image">
-                  <img src="assets/img/volunteer.webp" alt="" />
-                </div>
-                <div class="text">
-                  <h2 class="item-title">ボランティアさんと散歩へ</h2>
-                  <p class="date">2026.05.24</p>
-                  <p class="content">
-                    今日は大学生のボランティアさんがたくさん遊んでくれました！
-                  </p>
-                  <p class="link">続きを読む</p>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li class="item">
-            <a href="#">
-              <div class="grid inner">
-                <div class="image">
-                  <img src="assets/img/volunteer.webp" alt="" />
-                </div>
-                <div class="text">
-                  <h2 class="item-title">ボランティアさんと散歩へ</h2>
-                  <p class="date">2026.05.24</p>
-                  <p class="content">
-                    今日は大学生のボランティアさんがたくさん遊んでくれました！
-                  </p>
-                  <p class="link">続きを読む</p>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li class="item">
-            <a href="#">
-              <div class="grid inner">
-                <div class="image">
-                  <img src="assets/img/volunteer.webp" alt="" />
-                </div>
-                <div class="text">
-                  <h2 class="item-title">ボランティアさんと散歩へ</h2>
-                  <p class="date">2026.05.24</p>
-                  <p class="content">
-                    今日は大学生のボランティアさんがたくさん遊んでくれました！
-                  </p>
-                  <p class="link">続きを読む</p>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li class="item">
-            <a href="#">
-              <div class="grid inner">
-                <div class="image">
-                  <img src="assets/img/volunteer.webp" alt="" />
-                </div>
-                <div class="text">
-                  <h2 class="item-title">ボランティアさんと散歩へ</h2>
-                  <p class="date">2026.05.24</p>
-                  <p class="content">
-                    今日は大学生のボランティアさんがたくさん遊んでくれました！
-                  </p>
-                  <p class="link">続きを読む</p>
-                </div>
-              </div>
-            </a>
-          </li>
-          <li class="item">
-            <a href="#">
-              <div class="grid inner">
-                <div class="image">
-                  <img src="assets/img/volunteer.webp" alt="" />
-                </div>
-                <div class="text">
-                  <h2 class="item-title">ボランティアさんと散歩へ</h2>
-                  <p class="date">2026.05.24</p>
-                  <p class="content">
-                    今日は大学生のボランティアさんがたくさん遊んでくれました！
-                  </p>
-                  <p class="link">続きを読む</p>
-                </div>
-              </div>
-            </a>
-          </li>
-          <!-- delete later from to this point*************************************************-->
-        </ul>
-        <p class="pagenation">pagenation</p>
-      </div>
-    </main>
-    <!-- footer ----------------------->
-    <footer class="footer">
-      <div class="flex wrapper">
-        <div class="item">
-          <img
-            class="logo"
-            src="assets/img/RescueDog.png"
-            alt="レスキュードッグのロゴ"
-          />
-          <dl class="reception grid">
-            <dt>電話番号：</dt>
-            <dd><a href="tel:09000000000">090-0000-0000</a></dd>
-            <dt>受付時間：</dt>
-            <dd>午前9時～午後6時</dd>
-          </dl>
-        </div>
-        <nav id="footer-nav" class="footer-nav">
-          <ul>
-            <li><a href="#">トップページ</a></li>
-            <li><a href="#">ワンちゃん紹介</a></li>
-            <li><a href="#">譲渡の条件</a></li>
-            <li><a href="#">譲渡の流れ</a></li>
-            <li><a href="#">お知らせ一覧ページ</a></li>
-            <li><a href="#">私たちについて</a></li>
-            <li><a href="#">ご支援について</a></li>
-            <li><a href="#">ボランティアさん募集</a></li>
-            <li><a href="#">お問い合わせ</a></li>
-            <li><a href="#">収支報告一覧ページ</a></li>
-            <li><a href="#">プライバシーポリシー</a></li>
-          </ul>
-        </nav>
-      </div>
-      <p class="copyright">&copy;<span id="year"></span> RESCUE DOG</p>
-    </footer>
-    <script src="main.js"></script>
-  </body>
-</html>
+      <?php endwhile;
+      endif; ?>
+
+      <li class="item">
+        <a href="#">
+          <div class="grid inner">
+            <div class="image">
+              <img src="assets/img/volunteer.webp" alt="" />
+            </div>
+            <div class="text">
+              <h2 class="item-title">ボランティアさんと散歩へ</h2>
+              <p class="date">2026.05.24</p>
+              <p class="content">
+                今日は大学生のボランティアさんがたくさん遊んでくれました！
+              </p>
+              <p class="link">続きを読む</p>
+            </div>
+          </div>
+        </a>
+      </li>
+      <!-- delete later from here*************************************************-->
+      <li class="item">
+        <a href="#">
+          <div class="grid inner">
+            <div class="image">
+              <img src="assets/img/volunteer.webp" alt="" />
+            </div>
+            <div class="text">
+              <h2 class="item-title">ボランティアさんと散歩へ</h2>
+              <p class="date">2026.05.24</p>
+              <p class="content">
+                今日は大学生のボランティアさんがたくさん遊んでくれました！
+              </p>
+              <p class="link">続きを読む</p>
+            </div>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a href="#">
+          <div class="grid inner">
+            <div class="image">
+              <img src="assets/img/volunteer.webp" alt="" />
+            </div>
+            <div class="text">
+              <h2 class="item-title">ボランティアさんと散歩へ</h2>
+              <p class="date">2026.05.24</p>
+              <p class="content">
+                今日は大学生のボランティアさんがたくさん遊んでくれました！
+              </p>
+              <p class="link">続きを読む</p>
+            </div>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a href="#">
+          <div class="grid inner">
+            <div class="image">
+              <img src="assets/img/volunteer.webp" alt="" />
+            </div>
+            <div class="text">
+              <h2 class="item-title">ボランティアさんと散歩へ</h2>
+              <p class="date">2026.05.24</p>
+              <p class="content">
+                今日は大学生のボランティアさんがたくさん遊んでくれました！
+              </p>
+              <p class="link">続きを読む</p>
+            </div>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a href="#">
+          <div class="grid inner">
+            <div class="image">
+              <img src="assets/img/volunteer.webp" alt="" />
+            </div>
+            <div class="text">
+              <h2 class="item-title">ボランティアさんと散歩へ</h2>
+              <p class="date">2026.05.24</p>
+              <p class="content">
+                今日は大学生のボランティアさんがたくさん遊んでくれました！
+              </p>
+              <p class="link">続きを読む</p>
+            </div>
+          </div>
+        </a>
+      </li>
+      <li class="item">
+        <a href="#">
+          <div class="grid inner">
+            <div class="image">
+              <img src="assets/img/volunteer.webp" alt="" />
+            </div>
+            <div class="text">
+              <h2 class="item-title">ボランティアさんと散歩へ</h2>
+              <p class="date">2026.05.24</p>
+              <p class="content">
+                今日は大学生のボランティアさんがたくさん遊んでくれました！
+              </p>
+              <p class="link">続きを読む</p>
+            </div>
+          </div>
+        </a>
+      </li>
+      <!-- delete later from to this point*************************************************-->
+    </ul>
+    <p class="pagenation">pagenation</p>
+  </div>
+</main>
+<?php get_footer(); ?>
